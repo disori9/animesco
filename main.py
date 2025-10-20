1fd9b245b35a7c8a838d9b185b072ece533ce2fb
Ori <disori777@gmail.com>
2025-10-20 09:46:41 +0800

    Add user input to search for a show

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import requests
 
 
 base_url = "https://api.jikan.moe/v4/anime"
-
+# anime_title = input("Please input your title, accurate title produces accurate results: ")
+anime_title = "Attack on Titan"
+# q is basically the anime title, while limit limits the search results based on the integer given, default is 25
+# We need to create a params object do use the optional argument for requests' query parameters
 params = {
-    "q": "Attack on Titan",
-    "limit": 1
+    "q": anime_title,
+    "limit": 5
 }
 
 response = requests.get(base_url, params=params)
@@ -16,4 +19,18 @@ if response.status_code == 200:
 else:
     print(f"Request failed with status code: {response.status_code}")
     
-print(anime_info["data"])
+
+# The problem was basically how do I make sure that when the user searches for a show with an incomplete name, they'll get the result they want
+# The solution was to show the user the top 5 results with their corresponding index and then ask the user for what show they want, getting the index from them
+# Then getting the data from that index to have a variable which holds the show's data
+
+print("Search results:")
+for index, anime in enumerate(anime_info["data"]):
+    print(f"Anime [{index}]: {anime["title"]}")
+
+
+anime_selected = int(input("Please input the number of the title you want: "))
+anime_data = anime_info["data"][anime_selected]
+anime_score = anime_data["score"]
+
+print(f"{anime_data["title"]}: {anime_score}")
