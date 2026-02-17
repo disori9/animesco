0e19ff0ad82edf8abaa09bd22f87530a48bb2298
Ori <disori777@gmail.com>
2026-02-17 11:14:06 +0800

    Add more metadata Episode/Date

--- app.py
+++ app.py
@@ -25,6 +25,8 @@ def score():
         mal_score = mal_data.get("score")
         mal_cover = mal_data["images"]["jpg"]["image_url"]
         mal_status = mal_data["status"]
+        mal_episodes = mal_data["episodes"]
+        mal_date = mal_data["aired"]["string"]
         
         mal_genres = []
         for genre in mal_data["genres"]:
@@ -37,7 +39,8 @@ def score():
         average_score = (mal_score + anilist_score) / 2
         
         return render_template('selected_anime.html', anime_title=anime_title, average_score=average_score, mal_cover=mal_cover,
-                               mal_score=mal_score, anilist_score=anilist_score, synopsis=synopsis, mal_status=mal_status, mal_genres=mal_genres)
+                               mal_score=mal_score, anilist_score=anilist_score, synopsis=synopsis, mal_status=mal_status, mal_genres=mal_genres,
+                               mal_episodes=mal_episodes, mal_date=mal_date)
     except ValueError as e:
         return f"<h1>Error</h1> <p>{e}</p> <a href='/'>Go Back</a>"
     except Exception as e:
